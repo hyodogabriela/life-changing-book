471bc30a3c210162c0e8c48239272d93bc8c2b4e
hyodogabriela <gahyodo@gmail.com>
2023-12-13 16:34:58 +0100

    Added math.round in temperature, added description element and added some css

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ let apiKey = "f0308t4943329c9be1off0f74f2fa59a";
 let city = "Brooklyn";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
+
+
 axios.get(apiUrl)
   .then(response => {
+    let description = response.data.condition.description;
+    let descriptionElement = document.getElementById('descriptionElement');
+    descriptionElement.innerHTML = description;
     let temperature = response.data.temperature.current;
     let temperatureDisplay = document.getElementById('temperatureDisplay');
-    temperatureDisplay.innerHTML = temperature; 
+    temperatureDisplay.innerHTML = Math.round(temperature); 
   });
 //   .catch(error => {
 //     console.error('Error fetching temperature:', error);
